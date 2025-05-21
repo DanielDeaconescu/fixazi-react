7de7cf0885b379e8d724ca6539f1328b565cb7a9
Daniel Deaconescu <daniel.deaconescu23@gmail.com>
2025-05-21 17:02:18 +0300

    further changes

--- src/components/RepairForm.jsx
+++ src/components/RepairForm.jsx
@@ -118,7 +118,7 @@ export default function RepairForm() {
 
       // Close the modal
       const overlay = document.querySelector(".modal-backdrop");
-      overlay.style.opacity = "unset";
+      overlay.style.opacity = "0";
       navigate("/submitted");
 
       // Reset Turnstile after successful submission
