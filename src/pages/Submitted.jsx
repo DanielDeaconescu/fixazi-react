144f04e8fc123bd0625a4204388d1f8de30fee45
Daniel Deaconescu <daniel.deaconescu98@gmail.com>
2025-05-22 23:04:12 +0300

    further changes

--- src/pages/Submitted.jsx
+++ src/pages/Submitted.jsx
@@ -1,3 +1,5 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelopeCircleCheck } from "@fortawesome/free-solid-svg-icons";
 import Logo from "../components/Logo";
 import styled from "styled-components";
 
@@ -12,20 +14,26 @@ const StyledSection = styled.section`
   }
 `;
 
+const StyledParagraph = styled.p`
+  width: 70%;
+`;
+
+const StyledIcon = styled(FontAwesomeIcon)``;
+
 function Submitted() {
   return (
     <StyledSection className="bg-dark d-flex justify-content-center align-items-center">
       <div className="d-flex flex-column align-items-center text-center p-4 bg-dark rounded shadow-lg">
         <Logo />
         <h1 className="mb-3 text-success">
-          <i className="fa-solid fa-envelope-circle-check"></i>
+          <StyledIcon icon={faEnvelopeCircleCheck} className="me-2" />
           Cererea ta a fost trimisă!
         </h1>
-        <p className="fs-5 text-white p-submitted">
+        <StyledParagraph className="fs-5 text-white p-submitted">
           Am primit solicitarea dumneavoastră de reparație și vom reveni cu un
           răspuns cât mai curând posibil folosind metoda de contact selectată în
           formular. Vă mulțumim!
-        </p>
+        </StyledParagraph>
         <a href="/" className="btn btn-primary mt-4">
           Înapoi la pagina principală
         </a>
