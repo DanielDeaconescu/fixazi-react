e9cc5a52a50d014bc65c1b58630f9eb06d8f88da
Daniel Deaconescu <daniel.deaconescu23@gmail.com>
2025-05-25 23:32:02 +0300

    further changes

--- src/pages/TooManyRequests.jsx
+++ src/pages/TooManyRequests.jsx
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 import Logo from "../components/Logo";
 import { Link, useNavigate } from "react-router-dom";
+import { Helmet } from "react-helmet-async";
 
 const StyledSection = styled.section`
   height: 600px;
@@ -11,31 +12,40 @@ const StyledSection = styled.section`
 
 function TooManyRequests() {
   return (
-    <StyledSection>
-      <div className="container text-center">
-        <div className="card shadow-sm p-4">
-          <div className="card-body">
-            <div className="d-flex justify-content-center">
-              <Logo />
+    <>
+      <Helmet>
+        <title>Fixazi | Limită atinsă</title>
+        <meta
+          name="description"
+          content="Limită atinsă. Vă rugăm să încercați din nou în decurs de 24 de ore."
+        />
+      </Helmet>
+      <StyledSection>
+        <div className="container text-center">
+          <div className="card shadow-sm p-4">
+            <div className="card-body">
+              <div className="d-flex justify-content-center">
+                <Logo />
+              </div>
+              <h1 className="card-title text-danger mb-3">Limită atinsă</h1>
+              <p className="card-text fs-5 text-white">
+                Din motive de securitate, limităm numărul de cereri care pot fi
+                trimise într-o anumită perioadă de timp.
+              </p>
+              <p className="card-text fs-5 text-white">
+                Vă rugăm să încercați din nou în decurs de 24 de ore.
+              </p>
+              <p className="card-text fs-5 text-white">
+                Vă mulțumim pentru înțelegere!
+              </p>
+              <Link to="/" className="btn btn-primary mt-2">
+                Înapoi la pagina principală
+              </Link>
             </div>
-            <h1 className="card-title text-danger mb-3">Limită atinsă</h1>
-            <p className="card-text fs-5 text-white">
-              Din motive de securitate, limităm numărul de cereri care pot fi
-              trimise într-o anumită perioadă de timp.
-            </p>
-            <p className="card-text fs-5 text-white">
-              Vă rugăm să încercați din nou în decurs de 24 de ore.
-            </p>
-            <p className="card-text fs-5 text-white">
-              Vă mulțumim pentru înțelegere!
-            </p>
-            <Link to="/" className="btn btn-primary mt-2">
-              Înapoi la pagina principală
-            </Link>
           </div>
         </div>
-      </div>
-    </StyledSection>
+      </StyledSection>
+    </>
   );
 }
 
