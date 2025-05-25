d332ed4aca9e8a56744c031e853b34a61d738f0c
Daniel Deaconescu <daniel.deaconescu23@gmail.com>
2025-05-25 23:27:08 +0300

    further changes

--- src/pages/Cookies.jsx
+++ src/pages/Cookies.jsx
@@ -4,6 +4,13 @@ import CookiesInfo from "../components/CookiesInfo";
 function Cookies() {
   return (
     <>
+      <Helmet>
+        <title>Fixazi | Politica de cookies</title>
+        <meta
+          name="description"
+          content="Politica de cookies fixazi.com. fixazi.com nu folosește cookies momentan."
+        />
+      </Helmet>
       <CookiesHeader />
       <CookiesInfo />
     </>
